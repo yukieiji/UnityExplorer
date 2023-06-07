0383b74f3548bf6aafbc788dfefc4e0e12bdfe31
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-06-07 21:05:08 +0900

    update:versions

--- src/ExplorerCore.cs
+++ src/ExplorerCore.cs
@@ -19,7 +19,7 @@ namespace UnityExplorer
     public static class ExplorerCore
     {
         public const string NAME = "UnityExplorer";
-        public const string VERSION = "4.9.1";
+        public const string VERSION = "4.9.2";
         public const string AUTHOR = "Sinai, yukieiji";
         public const string GUID = "com.sinai.unityexplorer";
 
