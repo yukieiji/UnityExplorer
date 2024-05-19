6bf833079d32a9fb0f7b550db91b4e371a08a9eb
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-05-19 21:44:02 +0900

    refactor:error

--- src/ExplorerBehaviour.cs
+++ src/ExplorerBehaviour.cs
@@ -7,65 +7,69 @@ using Il2CppInterop.Runtime.Injection;
 #endif
 #endif
 
-namespace UnityExplorer
+namespace UnityExplorer;
+
+public class ExplorerBehaviour : MonoBehaviour
 {
-    public class ExplorerBehaviour : MonoBehaviour
+    internal static ExplorerBehaviour Instance { get; private set; }
+
+#if CPP
+    public ExplorerBehaviour(IntPtr ptr) : base(ptr) { }
+#endif
+
+    internal static void Setup()
     {
-        internal static ExplorerBehaviour Instance { get; private set; }
-
 #if CPP
-        public ExplorerBehaviour(System.IntPtr ptr) : base(ptr) { }
+        ClassInjector.RegisterTypeInIl2Cpp<ExplorerBehaviour>();
 #endif
 
-        internal static void Setup()
+        GameObject obj = new("ExplorerBehaviour");
+        DontDestroyOnLoad(obj);
+        obj.hideFlags = HideFlags.HideAndDontSave;
+        Instance = obj.AddComponent<ExplorerBehaviour>();
+    }
+
+    internal void Update()
+    {
+        ExplorerCore.Update();
+    }
+
+    // For editor, to clean up objects
+
+    internal void OnDestroy()
+    {
+        OnApplicationQuit();
+    }
+
+    internal bool quitting;
+
+    internal void OnApplicationQuit()
+    {
+        if (quitting) return;
+        quitting = true;
+        if (UIManager.UIRoot)
+            TryDestroy(UIManager.UIRoot.transform.root.gameObject);
+
+        TryDestroy((typeof(Universe).Assembly.GetType("UniverseLib.UniversalBehaviour")
+            .GetProperty("Instance", BindingFlags.Static | BindingFlags.NonPublic)
+            .GetValue(null, null)
+            as Component).gameObject);
+
+        TryDestroy(this.gameObject);
+    }
+
+    internal void TryDestroy(GameObject obj)
+    {
+        try
         {
-#if CPP
-            ClassInjector.RegisterTypeInIl2Cpp<ExplorerBehaviour>();
-#endif
-
-            GameObject obj = new("ExplorerBehaviour");
-            DontDestroyOnLoad(obj);
-            obj.hideFlags = HideFlags.HideAndDontSave;
-            Instance = obj.AddComponent<ExplorerBehaviour>();
-        }
-
-        internal void Update()
-        {
-            ExplorerCore.Update();
-        }
-
-        // For editor, to clean up objects
-
-        internal void OnDestroy()
-        {
-            OnApplicationQuit();
-        }
-
-        internal bool quitting;
-
-        internal void OnApplicationQuit()
-        {
-            if (quitting) return;
-            quitting = true;
-            if (UIManager.UIRoot)
-                TryDestroy(UIManager.UIRoot.transform.root.gameObject);
-
-            TryDestroy((typeof(Universe).Assembly.GetType("UniverseLib.UniversalBehaviour")
-                .GetProperty("Instance", BindingFlags.Static | BindingFlags.NonPublic)
-                .GetValue(null, null)
-                as Component).gameObject);
-
-            TryDestroy(this.gameObject);
-        }
-
-        internal void TryDestroy(GameObject obj)
-        {
-            try
+            if (obj)
             {
-                if (obj)
-                    Destroy(obj);
+                Destroy(obj);
             }
-            catch { }
+        }
+        catch (Exception e)
+        {
+            ExplorerCore.LogError($"Destroy Error!! {e.Message}");
         }
     }
 }
