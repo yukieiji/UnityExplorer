111ee75d7b9aaf9a882132d1233f19d00c41cccf
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-04-19 01:00:52 +0900

    bump up : version

--- src/ExplorerCore.cs
+++ src/ExplorerCore.cs
@@ -21,7 +21,7 @@ namespace UnityExplorer;
 public static class ExplorerCore
 {
     public const string NAME = "UnityExplorer";
-    public const string VERSION = "4.12.6";
+    public const string VERSION = "4.12.7";
     public const string AUTHOR = "Sinai, yukieiji";
     public const string GUID = "com.sinai.unityexplorer";
 
